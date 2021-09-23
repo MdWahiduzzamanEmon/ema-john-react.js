230d7495da280d4fa11625d9ab1c3f77d4bc62d7
AfianAvro <wahedemon09@gmail.com>
2021-09-23 16:14:24 +0600

    price add

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Cart = (props) => {
+    const carts = props.cart;
+    let initPrice = 0;
+    for (const price of carts) {
+        
+        initPrice = initPrice + price.price;
+        console.log(initPrice);
+    }
     return (
       <div>
         <h3>Order Summary</h3>
         <h5>Items ordered: {props.cart.length} </h5>
+        <h2>Price: ${initPrice} </h2>
       </div>
     );
 };
